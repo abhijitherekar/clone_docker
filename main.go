9959a75bf6b342d2f85e5b06fd9861e123bbc53f
aherekar <abhijit.herekar@oath.com>
2019-01-22 00:31:30 -0800

    Add re-exec command

--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"syscall"
+
+	"github.com/docker/docker/pkg/reexec"
 )
 
-func main() {
+func init() {
+	reexec.Register("hninit", hninit)
+	if reexec.Init() {
+		os.Exit(0)
+	}
+}
+
+func hninit() {
+	fmt.Println("Setting hostname")
+	syscall.Sethostname([]byte("check123"))
+	run()
+}
+
+func run() {
 	cmd := exec.Command("/bin/sh")
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 
 	cmd.Env = []string{"PS1=abhi-doc#"}
+	fmt.Println("in run function")
+	if err := cmd.Run(); err != nil {
+		log.Fatalln("Error while running cmd")
+	}
+}
+func main() {
+	cmd := reexec.Command("hninit")
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	//cmd.Env = []string{"PS1=abhi-doc#"}
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWNS |
@@ -38,7 +67,8 @@ func main() {
 			},
 		},
 	}
+	fmt.Println("11111: Set the uid, gid now cmd.RUN from mainsss")
 	if err := cmd.Run(); err != nil {
-		log.Fatalln("Error while running cmd")
+		log.Fatalln("Error while running reexec cmd")
 	}
 }
